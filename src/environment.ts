a9c643580e55a6f1cb61567cfe187e1f020da2d4
cillian00 <20094972@mail.wit.ie>
2024-04-28 17:07:13 +0100

    Removed sensitive info

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,12 @@
-export const SECRET_KEY: string = 'AKIA3UF3XMLOWRVTO2GV'
-export const SUPER_SECRET_KEY: string ='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'
-export const UserPoolId: string = 'eu-west-1_hfiyznUDM'
-export const ClientPoolId: string = '3m71vm63gp9jsmnro3rdl5ttof'
+export const SECRET_KEY: string = ''
+export const SUPER_SECRET_KEY: string = ''
+export const UserPoolId: string = ''
+export const ClientPoolId: string = ''
 
 export const environment = {
-
   production: false,
-    cognito: {
-       userPoolId: UserPoolId,
-       userPoolWebClientId: ClientPoolId
+  cognito: {
+    userPoolId: UserPoolId,
+    userPoolWebClientId: ClientPoolId
   }
 }
